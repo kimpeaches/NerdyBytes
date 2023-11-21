898b4e1754b38315dba3a017e8c1cd50b1f92f0c
kim <kreyes@kimchi.consulting>
2023-11-21 11:39:26 -0800

    updated docker-compose.yaml file

--- api/main.py
+++ api/main.py
@@ -1,7 +1,24 @@
+import os
 from fastapi import FastAPI
 from routers import user
 from authenticator import authenticator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    os.environ.get("CORS_HOST", "http://localhost"),
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(user.router)
 app.include_router(authenticator.router)
