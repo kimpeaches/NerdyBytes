ead59e3ffa775ea15753a87a723ba92e4d31042d
Kim Reyes <kreyes@kimchi.consulting>
2023-11-22 21:48:41 +0000

    Login frontend component

--- api/authenticator.py
+++ api/authenticator.py
@@ -10,25 +10,18 @@ class MyAuthenticator(Authenticator):
         username: str,
         accounts: UserRepository,
     ):
-        # Use your repo to get the account based on the
-        # username (which could be an email)
         return accounts.get(username)
 
     def get_account_getter(
         self,
         accounts: UserRepository = Depends(),
     ):
-        # Return the accounts. That's it.
         return accounts
 
     def get_hashed_password(self, account: UserOutWithPassword):
-        # Return the encrypted password value from your
-        # account object
         return account.hashed_password
 
     def get_account_data_for_cookie(self, account: UserOut):
-        # Return the username and the data for the cookie.
-        # You must return TWO values from this method.
         return account.username, UserOut(**account.dict())
 
 
--- api/routers/user.py
+++ api/routers/user.py
@@ -13,7 +13,13 @@ from authenticator import authenticator
 
 from pydantic import BaseModel
 
-from queries.user import UserIn, UserOut, UserRepository, DuplicateAccountError
+from queries.user import (
+    LoginUserIn,
+    UserIn,
+    UserOut,
+    UserRepository,
+    DuplicateAccountError,
+)
 
 
 class UserForm(BaseModel):
@@ -31,6 +37,16 @@ class HttpError(BaseModel):
 
 
 router = APIRouter()
+
+
+@router.get("/api/user/{id}")
+async def get_user(
+    id: int,
+    accounts: UserRepository = Depends(),
+    _=Depends(authenticator.get_current_account_data),
+) -> UserOut:
+    account = accounts.get_user_by_id(id)
+    return account
 
 
 @router.post("/api/user", response_model=UserToken | HttpError)
@@ -57,3 +73,31 @@ async def create_User(
 
     token = await authenticator.login(response, request, form, repo)
     return UserToken(account=user, **token.dict())
+
+
+@router.post("/token", response_model=UserToken)
+async def post_token(
+    request: Request,
+    response: Response,
+    repo: UserRepository = Depends(),
+    form: LoginUserIn = Depends(LoginUserIn.as_form),
+):
+    token = await authenticator.login(response, request, form, repo)
+    user = repo.get(form.username)
+    return UserToken(account=user, **token.dict())
+
+
+@router.get("/token")
+async def get_by_cookie(
+    request: Request,
+    account_data: dict
+    | None = Depends(authenticator.try_get_current_account_data),
+    accounts: UserRepository = Depends(),
+    ra=Depends(authenticator.get_current_account_data),
+) -> UserToken:
+    account = await get_user(account_data["id"], accounts=accounts)
+    return {
+        "access_token": request.cookies[authenticator.cookie_name],
+        "type": "Bearer",
+        "account": account,
+    }
