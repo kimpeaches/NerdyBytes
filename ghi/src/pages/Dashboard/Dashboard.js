45606b8e6d562b663ac25bd97292d8d29ee86850
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-01 21:26:22 -0800

    Style structure for dashboard components.

--- ghi/src/pages/Dashboard/Dashboard.js
+++ ghi/src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-
 import "./Dashboard.css";
+import UserProfile from "../../components/dashboard/UserProfile/UserProfile";
+import Calendar from "../../components/dashboard/Calendar/Calendar";
+import UserDecks from "../../components/dashboard/UserDecks/UserDecks";
 
 function Dashboard() {
   return (
     <div className="dashboard-container">
-      <h1>Dashboard</h1>
-      <Routes></Routes>
+      <h1>#1 This is the Dashboard Component</h1>
+      <div className="d-flex">
+        <UserProfile />
+        <Calendar />
+      </div>
+      <UserDecks />
     </div>
   );
 }
