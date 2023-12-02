2a9f764b4e76f96b10dca1fd8cfb589ec4db36bf
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-01 20:54:01 -0800

    Add file structure for dashboard. Move Nav into new structure and ensure imports still work.

---
+++ ghi/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default Dashboard;
