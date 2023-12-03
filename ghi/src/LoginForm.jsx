2a162d537deaa6396500f18a02d549607e6f7302
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-02 17:37:06 -0800

    Fix invalid for and class usage in jsx.

--- ghi/src/LoginForm.jsx
+++ ghi/src/LoginForm.jsx
@@ -3,67 +3,65 @@ import { useState } from "react";
 import "./Login.css";
 
 const LoginForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const { login } = useToken();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        try {
-            const token = login(username, password);
-            if (!token) {
-                throw new Error(
-                    "Failed to get token after login. Got undefined"
-                );
-            }
-        } catch (error) {
-            console.error(error);
-        }
-        e.target.reset();
-    };
-    return (
-        <>
-            <div>
-                <h2 className="title">NerdyBytes</h2>
-                <h4 className="motto">
-                    Unleash your potential, one card at a time.
-                </h4>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useToken();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const token = login(username, password);
+      if (!token) {
+        throw new Error("Failed to get token after login. Got undefined");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    e.target.reset();
+  };
+
+  return (
+    <>
+      <div>
+        <h2 className="title">NerdyBytes</h2>
+        <h4 className="motto">Unleash your potential, one card at a time.</h4>
+      </div>
+      <div className="login">
+        <div>
+          <div className="owl">
+            <div className="hand"></div>
+            <div className="hand hand-r"></div>
+            <div className="arms">
+              <div className="arm"></div>
+              <div className="arm arm-r"></div>
             </div>
-            <div className="login">
-                <div>
-                    <div className="owl">
-                        <div className="hand"></div>
-                        <div className="hand hand-r"></div>
-                        <div className="arms">
-                            <div className="arm"></div>
-                            <div className="arm arm-r"></div>
-                        </div>
-                    </div>
-                    <form onSubmit={(e) => handleSubmit(e)} className="form">
-                        <div className="control">
-                            <label for="username"></label>
-                            <input
-                                id="username"
-                                placeholder="Username"
-                                type="text"
-                                onChange={(e) => setUsername(e.target.value)}
-                            ></input>
-                        </div>
-                        <div className="control">
-                            <label for="password"></label>
-                            <input
-                                id="password"
-                                placeholder="Password"
-                                type="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></input>
-                        </div>
-                        <div class="wrapper">
-                            <button>Login</button>
-                        </div>
-                    </form>
-                </div>
+          </div>
+          <form onSubmit={(e) => handleSubmit(e)} className="form">
+            <div className="control">
+              <label htmlFor="username"></label>
+              <input
+                id="username"
+                placeholder="Username"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
             </div>
-        </>
-    );
+            <div className="control">
+              <label htmlFor="password"></label>
+              <input
+                id="password"
+                placeholder="Password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <div className="wrapper">
+              <button>Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 export default LoginForm;
