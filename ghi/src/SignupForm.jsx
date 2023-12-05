681f3cd869e3576eef2d70e59f31096e6f2afeb0
Harold Sy <harold.a.sy@gmail.com>
2023-12-04 18:54:17 -0800

    frontend for sign up

---
+++ ghi/src/SignupForm.jsx
@@ -0,0 +1,67 @@
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useState } from "react";
+import "./Signup.css";
+import { useNavigate } from "react-router-dom";
+
+const SignupForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const { login } = useToken();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await login(username, password);
+            navigate("/dashboard");
+        } catch (error) {
+            console.error("Signup failed: ", error);
+        }
+    };
+
+    return (
+        <>
+            <div>
+                <h1 className="title">NerdyBytes</h1>
+                <h4 className="motto">Unleash your potential, one card at a time.</h4>
+            </div>
+            <div className="signup">
+                <div>
+                    <div className="owl">
+                        <div className="hand"></div>
+                        <div className="hand hand-r"></div>
+                        <div className="arms">
+                            <div className="arm"></div>
+                            <div className="arm arm-r"></div>
+                        </div>
+                    </div>
+                    <form onSubmit={(e) => handleSubmit(e)} className="form">
+                        <div className="control">
+                            <label htmlFor="username"></label>
+                            <input
+                                id="username"
+                                placeholder="Username"
+                                type="text"
+                                onChange={(e) => setUsername(e.target.value)}
+                            ></input>
+                        </div>
+                        <div className="control">
+                            <label htmlFor="password"></label>
+                            <input
+                                id="password"
+                                placeholder="Password"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            ></input>
+                        </div>
+                        <div className="signup-btn wrapper">
+                            <button>SignUp</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+export default SignupForm;
