1c714196fea698b31f8fe0682ae099a3a0ffd4f3
Kim Reyes <kreyes@kimchi.consulting>
2023-12-05 02:20:43 +0000

    Implement useContext

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,32 +2,60 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import Nav from "./components/shared/Nav/Nav";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import LoginForm from "./LoginForm";
+import LoginForm from "./pages/Login/LoginForm";
 import "./App.css";
-import Chat from "./ChatPage";
+import Chat from "./pages/Chat/ChatPage";
+import { UserProvider } from "./UserContext";
+import { useState, useEffect } from "react";
 
 function App() {
     const domain = /https:\/\/[^/]+/;
     const basename = process.env.PUBLIC_URL.replace(domain, "");
+    const [users, setUsers] = useState([]);
+    const [currentUser, setCurrentUser] = useState(null);
 
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch(
+                    "http://localhost:8000/api/users/",
+                    {
+                        credentials: "include",
+                    }
+                );
+                const userData = await response.json();
+                setUsers(userData);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
     return (
         <div className="container">
             <BrowserRouter basename={basename}>
                 <Nav />
                 <AuthProvider baseUrl="http://localhost:8000">
-                    <Routes>
-                        <Route
-                            exact
-                            path="/login"
-                            element={<LoginForm />}
-                        ></Route>
-                        <Route
-                            exact
-                            path="/dashboard"
-                            element={<Dashboard />}
-                        ></Route>
-                        <Route exact path="/chat" element={<Chat />}></Route>
-                    </Routes>
+                    <UserProvider currentUser={currentUser}>
+                        <Routes>
+                            <Route
+                                exact
+                                path="/login"
+                                element={<LoginForm />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/dashboard"
+                                element={<Dashboard />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/chat"
+                                element={<Chat />}
+                            ></Route>
+                        </Routes>
+                    </UserProvider>
                 </AuthProvider>
             </BrowserRouter>
         </div>
