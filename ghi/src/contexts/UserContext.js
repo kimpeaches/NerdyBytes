845ce95cd92d2b101e6325f4dab9522dcf874057
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-04 16:45:28 -0800

    Add persist.

--- ghi/src/contexts/UserContext.js
+++ ghi/src/contexts/UserContext.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 
-const UserContext = React.createContext();
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const value = {
+    user,
+    login: (userData) => {
+      localStorage.setItem("user", JSON.stringify(userData));
+      setUser(userData);
+    },
+    logout: () => {
+      localStorage.removeItem("user");
+      setUser(null);
+    },
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
 
 export default UserContext;
