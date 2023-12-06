9c4862b2191e5e8331b59eefadc80d7c85f8c1e2
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-06 10:40:48 -0800

    Fix App.js.

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,18 +3,14 @@ import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import Nav from "./components/shared/Nav/Nav";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import LoginForm from "./pages/Login/LoginForm";
-import SignupForm from "./pages/Signup/SignupForm";
 import "./App.css";
 import Chat from "./pages/Chat/ChatPage";
 import { UserProvider } from "./useContext/UserContext";
 import ChatRoomContext from "./useContext/ChatRoomContext";
 import { useState, useEffect } from "react";
+import SignupForm from "./pages/Signup/SignupForm";
 
 function App() {
-  const domain = /https:\/\/[^/]+/;
-  const basename = process.env.PUBLIC_URL.replace(domain, "");
-  const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
   const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL.replace(domain, "");
   const [users, setUsers] = useState([]);
@@ -33,19 +29,36 @@ function App() {
       }
     };
 
+    const fetchRoom = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/rooms/", {
+          credentials: "include",
+        });
+        const roomData = await response.json();
+        setChatRoomId(roomData.id);
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+      }
+    };
+
     fetchUsers();
+    fetchRoom();
   }, []);
+
   return (
     <div className="container">
       <BrowserRouter basename={basename}>
         <Nav />
         <AuthProvider baseUrl="http://localhost:8000">
-          <UserProvider currentUser={currentUser}>
-            <Routes>
-              <Route exact path="/login" element={<LoginForm />}></Route>
-              <Route exact path="/dashboard" element={<Dashboard />}></Route>
-              <Route exact path="/chat" element={<Chat />}></Route>
-            </Routes>
+          <UserProvider currentUser={users}>
+            <ChatRoomContext.Provider value={chatRoomId}>
+              <Routes>
+                <Route exact path="/" element={<LoginForm />}></Route>
+                <Route exact path="/dashboard" element={<Dashboard />}></Route>
+                <Route exact path="/chat" element={<Chat />}></Route>
+                <Route exact path="/signup" element={<SignupForm />}></Route>
+              </Routes>
+            </ChatRoomContext.Provider>
           </UserProvider>
         </AuthProvider>
       </BrowserRouter>
