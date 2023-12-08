9f9967ed78ef7c2feeb0b90ef4172f7e280b6b31
kim <kreyes@kimchi.consulting>
2023-12-07 16:37:24 -0800

    deck-list partially complete, more css to be added

--- ghi/src/pages/Deck/PublicDeck.js
+++ ghi/src/pages/Deck/PublicDeck.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useUserContext } from "../../UserContext";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import TextField from "@mui/material/TextField";
 
 function PublicDeck() {
     const user_id = useUserContext();
     const [decks, setDecks] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -21,6 +26,12 @@ function PublicDeck() {
         fetchData();
     }, [user_id]);
 
+    const handleChange = (e) => setSearchInput(e.target.value.toLowerCase());
+
+    const filteredDeck = decks
+        ? decks.filter((deck) => deck.name.toLowerCase().includes(searchInput))
+        : decks;
+
     const cardStyle = {
         transition: "transform 0.2s",
         background: "transparent",
@@ -33,6 +44,27 @@ function PublicDeck() {
 
     return (
         <div className="container">
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <List>
+                    <ListItem key="SearchBar">
+                        <Grid item xs={12} style={{ padding: "20px" }}>
+                            <TextField
+                                label="Search"
+                                fullWidth
+                                onChange={handleChange}
+                                style={{ width: "650px" }}
+                            />
+                        </Grid>
+                    </ListItem>
+                </List>
+            </div>
             <h2
                 className="text-center"
                 style={{
@@ -44,56 +76,68 @@ function PublicDeck() {
             >
                 Decks
             </h2>
-            <div className="row justify-content-center">
-                {decks &&
-                    decks
-                        .filter((deck) => deck.public_status)
-                        .map((deck) => (
-                            <div
-                                key={deck.id}
-                                className="col-md-3 mb-4"
-                                style={{
-                                    ...cardStyle,
-                                    border: `2px solid var(--primary)`,
-                                    borderRadius: "10px",
-                                    overflow: "hidden",
-                                }}
-                                onMouseEnter={(e) => {
-                                    e.currentTarget.style.transform =
-                                        hoverStyle.transform;
-                                }}
-                                onMouseLeave={(e) => {
-                                    e.currentTarget.style.transform = "";
-                                }}
-                            >
+            <List>
+                <div className="row">
+                    {filteredDeck &&
+                        filteredDeck
+                            .filter((deck) => deck.public_status)
+                            .map((deck) => (
                                 <div
-                                    className="card"
+                                    key={deck.id}
+                                    className="col-md-3 mb-4"
                                     style={{
-                                        backgroundColor: "var(--background)",
+                                        ...cardStyle,
+                                        border: `2px solid var(--primary)`,
+                                        borderRadius: "10px",
+                                        overflow: "hidden",
+                                        margin: "10px",
+                                    }}
+                                    onMouseEnter={(e) => {
+                                        e.currentTarget.style.transform =
+                                            hoverStyle.transform;
+                                    }}
+                                    onMouseLeave={(e) => {
+                                        e.currentTarget.style.transform = "";
                                     }}
                                 >
-                                    <div className="card-body">
-                                        <h5 className="card-title">
-                                            <a
-                                                href="/"
-                                                style={{
-                                                    color: "var(--primary)",
-                                                }}
-                                            >
-                                                {deck.name}
-                                            </a>
-                                        </h5>
-                                    </div>
-                                    <p
-                                        className="card-text"
-                                        style={{ color: "var(--text)" }}
+                                    <div
+                                        className="card"
+                                        style={{
+                                            backgroundColor:
+                                                "var(--background)",
+                                            height: "100%",
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            justifyContent: "space-between",
+                                        }}
                                     >
-                                        New Card: {deck.total_cards}
-                                    </p>
+                                        <div
+                                            className="card-body text-center"
+                                            style={{ flex: "1" }}
+                                        >
+                                            <h5 className="card-title">
+                                                <a
+                                                    href="/"
+                                                    style={{
+                                                        color: "var(--primary)",
+                                                        textDecoration: "none",
+                                                    }}
+                                                >
+                                                    {deck.name}
+                                                </a>
+                                            </h5>
+                                        </div>
+                                        <p
+                                            className="card-text text-center"
+                                            style={{ color: "var(--text)" }}
+                                        >
+                                            New Card: {deck.total_cards}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-            </div>
+                            ))}
+                </div>
+            </List>
         </div>
     );
 }
