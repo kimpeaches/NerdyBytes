62f0639d31c88dc166f39014b21052f00ec36aa0
Kim Reyes <kreyes@kimchi.consulting>
2023-12-11 19:58:50 +0000

    Style branch

--- ghi/src/pages/Chat/ChatRoom.js
+++ ghi/src/pages/Chat/ChatRoom.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
@@ -10,55 +9,55 @@ import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-
 export default function ChatRoom() {
-  const { chatRoomId = 1 } = useParams();
-  const [searchRooms, setSearchRooms] = useState([]);
-  const [searchText, setSearchText] = useState("");
-
-  const fetchRooms = async () => {
-    const res = await fetch(`${process.env.REACT_APP_API_HOST}/api/rooms/`, {
-      credentials: "include",
-    });
-    const response = await res.json();
-    setSearchRooms(response ?? []);
-  };
-
-  useEffect(() => {
-    fetchRooms();
-    setInterval(fetchRooms, 60000);
-  }, []);
-
-  const handleChange = (e) => setSearchText(e.target.value);
-
-  const filteredRooms = searchText
-    ? searchRooms.filter((room) =>
-        room.name.toLowerCase().includes(searchText.toLowerCase())
-      )
-    : searchRooms;
-
-  return (
-    <>
-      <List>
-        <ListItem key="SearchBar">
-          <Grid item xs={12} style={{ padding: "10px" }}>
-            <TextField label="Search" fullWidth onChange={handleChange} />
-          </Grid>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        {filteredRooms.map((room) => (
-          <Link to={`/chat/${room.id}`}>
-            <ListItemButton
-              key={room.id}
-              selected={parseInt(chatRoomId) === room.id}
-            >
-              <ListItemText primary={room.name} />
-            </ListItemButton>
-          </Link>
-        ))}
-      </List>
-    </>
-  );
+    const { chatRoomId = 1 } = useParams();
+    const [searchRooms, setSearchRooms] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const fetchRooms = async () => {
+        const res = await fetch(
+            `${process.env.REACT_APP_API_HOST}/api/rooms/`,
+            {
+                credentials: "include",
+            }
+        );
+        const response = await res.json();
+        setSearchRooms(response ?? []);
+    };
+    useEffect(() => {
+        fetchRooms();
+        setInterval(fetchRooms, 60000);
+    }, []);
+    const handleChange = (e) => setSearchText(e.target.value);
+    const filteredRooms = searchText
+        ? searchRooms.filter((room) =>
+              room.name.toLowerCase().includes(searchText.toLowerCase())
+          )
+        : searchRooms;
+    return (
+        <>
+            <List>
+                <ListItem key="SearchBar">
+                    <Grid item xs={12} style={{ padding: "10px" }}>
+                        <TextField
+                            label="Search"
+                            fullWidth
+                            onChange={handleChange}
+                        />
+                    </Grid>
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                {filteredRooms.map((room) => (
+                    <Link key={room.id} to={`/chat/${room.id}`}>
+                        <ListItemButton
+                            selected={parseInt(chatRoomId) === room.id}
+                        >
+                            <ListItemText primary={room.name} />
+                        </ListItemButton>
+                    </Link>
+                ))}
+            </List>
+        </>
+    );
 }
