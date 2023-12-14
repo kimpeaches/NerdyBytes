08adffdb5eadcfe1aba1ce8130330e5fd12eecda
kim <kreyes@kimchi.consulting>
2023-12-13 20:37:57 -0800

    fixed es lint issue

--- ghi/src/pages/CardList/CardList.js
+++ ghi/src/pages/CardList/CardList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import CardForm from "../../components/cardlist/Card/CreateCard";
 import "./CardList.css";
 import "../Login/Login.css";
