c94708f00003f3f28ee86f04d746daa74facf5a7
Kim Reyes <kreyes@kimchi.consulting>
2023-12-14 05:59:13 +0000

    Updated CSS style

--- ghi/src/components/shared/Nav/Nav.js
+++ ghi/src/components/shared/Nav/Nav.js
@@ -3,84 +3,80 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  async function logoutUser() {
-    const url = `${process.env.REACT_APP_API_HOST}/token`;
-    const fetchOptions = {
-      credentials: "include",
-      method: "DELETE",
-    };
-    const response = await fetch(url, fetchOptions);
-    if (!response.ok) {
-      console.log("An error occurred during logout");
+    async function logoutUser() {
+        const url = `${process.env.REACT_APP_API_HOST}/token`;
+        const fetchOptions = {
+            credentials: "include",
+            method: "DELETE",
+        };
+        const response = await fetch(url, fetchOptions);
+        if (!response.ok) {
+            console.log("An error occurred during logout");
+        }
     }
-  }
 
-  function handleLogout() {
-    logoutUser();
-    setTimeout(() => {
-      navigate("/");
-    }, 250);
-  }
+    function handleLogout() {
+        logoutUser();
+        setTimeout(() => {
+            navigate("/");
+        }, 250);
+    }
 
-  if (location.pathname === "/" || location.pathname === "/signup") {
-    return <></>;
-  }
+    if (location.pathname === "/" || location.pathname === "/signup") {
+        return <></>;
+    }
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary p-2">
-      <div className="navbar-container">
-        <Link className="navbar-brand" to="/dashboard">
-          <img
-            className="logo rounded-circle white-border"
-            src="http://grapplingwithsyntax.com/side_scroll_paralax/NerdyBytesLogo.jpg"
-            alt="Nerdybytes Logo"
-            width="100"
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <ul className="navbar-nav navbar-list">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/chat/1">
-              Chat
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/publicdeck">
-              Public List
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/create-deck">
-              Create Deck
-            </Link>
-          </li>
-          <li className="nav-item" onClick={handleLogout}>
-            <div className="nav-link" style={{ cursor: "pointer" }}>
-              Logout
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary p-2">
+            <div className="navbar-container">
+                <Link className="navbar-brand" to="/dashboard">
+                    <img
+                        className="logo rounded-circle white-border"
+                        src={process.env.PUBLIC_URL + "/NerdybytesLogo.jpg"}
+                        alt="Nerdybytes Logo"
+                        width="100"
+                    />
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <ul className="navbar-nav navbar-list">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/dashboard">
+                            Dashboard
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/publicdeck">
+                            Public List
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create-deck">
+                            Create Deck
+                        </Link>
+                    </li>
+                    <li className="nav-item" onClick={handleLogout}>
+                        <div className="nav-link" style={{ cursor: "pointer" }}>
+                            Logout
+                        </div>
+                    </li>
+                </ul>
             </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Nav;
--- ghi/src/pages/CreateOption/AddOption.js
+++ ghi/src/pages/CreateOption/AddOption.js
@@ -1,233 +1,244 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import "./AddOption.css";
 
 function AddOption() {
-  const { cardId } = useParams();
-  const [card, setCard] = useState({});
-  const [option, setOption] = useState("");
-  const [options, setOptions] = useState([]);
-  const [reload, setReload] = useState(false);
-  const [optionCreated, setOptionCreated] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
+    const { cardId } = useParams();
+    const [card, setCard] = useState({});
+    const [option, setOption] = useState("");
+    const [options, setOptions] = useState([]);
+    const [reload, setReload] = useState(false);
+    const [optionCreated, setOptionCreated] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
-  const handleInputChange = (e) => {
-    setIsChecked(e.target.checked);
-  };
-
-  const handleDelete = async (id) => {
-    const url = `${process.env.REACT_APP_API_HOST}/api/option/${id}`;
-    const fetchOptions = {
-      method: "DELETE",
-      credentials: "include",
+    const handleInputChange = (e) => {
+        setIsChecked(e.target.checked);
     };
 
-    try {
-      const response = await fetch(url, fetchOptions);
-      if (response.ok) {
-        setOptions((prevOptions) =>
-          prevOptions.filter((option) => option.id !== id)
-        );
-      } else {
-        console.log("Error deleting option");
-      }
-    } catch (error) {
-      console.log("Error: " + error);
-    }
-  };
-
-  const handleOptionChange = (e) => {
-    setOption(e.target.value);
-    setOptionCreated(false);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError(null);
-
-    const data = {
-      card_id: parseInt(cardId, 10),
-      possible_answer: option,
-      is_correct: isChecked,
-    };
-
-    const url = `${process.env.REACT_APP_API_HOST}/api/option`;
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-    };
-
-    try {
-      const response = await fetch(url, fetchOptions);
-      if (response.ok) {
-        const locationHeader = response.headers.get("location");
-        const newOption = {
-          id: locationHeader ? locationHeader.split("/").pop() : null,
-          possible_answer: option,
-          is_correct: isChecked,
+    const handleDelete = async (id) => {
+        const url = `${process.env.REACT_APP_API_HOST}/api/option/${id}`;
+        const fetchOptions = {
+            method: "DELETE",
+            credentials: "include",
         };
-        setOptions((prevOptions) => [...prevOptions, newOption]);
-        setOption("");
-        setIsChecked(false);
-        setOptionCreated(true);
-        setReload(!reload);
 
-        fetchOptionsList();
-      } else {
-        setError("An error occurred while creating the option");
-      }
-    } catch (error) {
-      setError("Error: " + error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const fetchOptionsList = async () => {
-    const url = `${process.env.REACT_APP_API_HOST}/api/${cardId}/option`;
-    try {
-      const response = await fetch(url, {
-        credentials: "include",
-        method: "GET",
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setOptions(data);
-      } else {
-        console.log("No options found for this card");
-      }
-    } catch (error) {
-      console.log("Error fetching options:", error);
-    }
-  };
-
-  useEffect(() => {
-    async function getCard() {
-      const url = `${process.env.REACT_APP_API_HOST}/api/card/${cardId}`;
-      try {
-        const response = await fetch(url, {
-          credentials: "include",
-          method: "GET",
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setCard(data);
-        } else {
-          console.log("No card found with provided ID");
+        try {
+            const response = await fetch(url, fetchOptions);
+            if (response.ok) {
+                setOptions((prevOptions) =>
+                    prevOptions.filter((option) => option.id !== id)
+                );
+            } else {
+                console.log("Error deleting option");
+            }
+        } catch (error) {
+            console.log("Error: " + error);
         }
-      } catch (error) {
-        console.log("Error fetching card:", error);
-      } finally {
-        setLoading(false);
-      }
+    };
+
+    const handleOptionChange = (e) => {
+        setOption(e.target.value);
+        setOptionCreated(false);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+
+        const data = {
+            card_id: parseInt(cardId, 10),
+            possible_answer: option,
+            is_correct: isChecked,
+        };
+
+        const url = `${process.env.REACT_APP_API_HOST}/api/option`;
+        const fetchOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+            credentials: "include",
+        };
+
+        try {
+            const response = await fetch(url, fetchOptions);
+            if (response.ok) {
+                const locationHeader = response.headers.get("location");
+                const newOption = {
+                    id: locationHeader ? locationHeader.split("/").pop() : null,
+                    possible_answer: option,
+                    is_correct: isChecked,
+                };
+                setOptions((prevOptions) => [...prevOptions, newOption]);
+                setOption("");
+                setIsChecked(false);
+                setOptionCreated(true);
+                setReload(!reload);
+
+                fetchOptionsList();
+            } else {
+                setError("An error occurred while creating the option");
+            }
+        } catch (error) {
+            setError("Error: " + error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchOptionsList = async () => {
+        const url = `${process.env.REACT_APP_API_HOST}/api/${cardId}/option`;
+        try {
+            const response = await fetch(url, {
+                credentials: "include",
+                method: "GET",
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setOptions(data);
+            } else {
+                console.log("No options found for this card");
+            }
+        } catch (error) {
+            console.log("Error fetching options:", error);
+        }
+    };
+
+    useEffect(() => {
+        async function getCard() {
+            const url = `${process.env.REACT_APP_API_HOST}/api/card/${cardId}`;
+            try {
+                const response = await fetch(url, {
+                    credentials: "include",
+                    method: "GET",
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setCard(data);
+                } else {
+                    console.log("No card found with provided ID");
+                }
+            } catch (error) {
+                console.log("Error fetching card:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getCard();
+        fetchOptionsList();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cardId]);
+
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
-    getCard();
-    fetchOptionsList();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardId]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <>
-      <div className="card-list">
-        <h1 className="mb-3 text-center">Question: {card.question}</h1>
-        <table className="table table-hover table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID #</th>
-              <th scope="col">Possible Answer</th>
-              <th scope="col">Correct</th>
-              <th scope="col">Edit</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {options.map((option) => (
-              <tr key={option.id}>
-                <td>{option.id}</td>
-                <td>{option.possible_answer}</td>
-                <td>{option.is_correct ? "Yes" : "No"}</td>
-                <td>
-                  <button
-                    onClick={() =>
-                      navigate(
-                        `/decks/${cardId}/cards/${option.id}/options/${option.id}/edit`
-                      )
-                    }
-                  >
-                    Edit
-                  </button>
-                </td>
-                <td>
-                  <button onClick={() => handleDelete(option.id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="card shadow m-5 p-3 col-6 mx-auto">
-        <div className="card-body">
-          <form onSubmit={handleSubmit} id="create-option-form">
-            <h1 className="card-title">
-              Add Possible Answer to Card: {card.question}
-            </h1>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                id="option"
-                value={option}
-                onChange={handleOptionChange}
-                required
-              />
-              {!option && <p>Please enter a Possible Answer.</p>}
+    return (
+        <>
+            <div className="card-list">
+                <h1 className="mb-3 text-center">Question: {card.question}</h1>
+                <table className="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID #</th>
+                            <th scope="col">Possible Answer</th>
+                            <th scope="col">Correct</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {options.map((option) => (
+                            <tr key={option.id}>
+                                <td>{option.id}</td>
+                                <td>{option.possible_answer}</td>
+                                <td>{option.is_correct ? "Yes" : "No"}</td>
+                                <td>
+                                    <button
+                                        id="buttons"
+                                        onClick={() =>
+                                            navigate(
+                                                `/decks/${cardId}/cards/${option.id}/options/${option.id}/edit`
+                                            )
+                                        }
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button
+                                        id="buttons"
+                                        onClick={() => handleDelete(option.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-            <div className="mt-2 d-flex">
-              <span className="mr-4">Correct?</span>
-              <input
-                className="ms-2"
-                type="checkbox"
-                id="isCorrect"
-                checked={isChecked}
-                onChange={handleInputChange}
-              />
-            </div>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-            <button type="submit" className="btn btn-primary mt-3">
-              Create Possible Answer
-            </button>
-          </form>
-        </div>
-      </div>
 
-      {optionCreated && (
-        <div className="alert alert-success p-2 col-6 mx-auto">
-          <p className="h3 text-center">Answer Successfully Created!</p>
-          <Link to="/dashboard" className="m-3 larger-text">
-            {"<< Go to Dashboard"}
-          </Link>
-        </div>
-      )}
-    </>
-  );
+            <div className="card shadow m-5 p-3 col-6 mx-auto">
+                <div className="card-body">
+                    <form onSubmit={handleSubmit} id="create-option-form">
+                        <h1 className="card-title">
+                            Add Possible Answer to Card: {card.question}
+                        </h1>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="option"
+                                value={option}
+                                onChange={handleOptionChange}
+                                required
+                            />
+                            {!option && <p>Please enter a Possible Answer.</p>}
+                        </div>
+                        <div className="mt-2 d-flex">
+                            <span className="mr-4">Correct?</span>
+                            <input
+                                className="ms-2"
+                                type="checkbox"
+                                id="isCorrect"
+                                checked={isChecked}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        {loading && <p>Loading...</p>}
+                        {error && <p>Error: {error}</p>}
+                        <button
+                            id="buttons"
+                            type="submit"
+                            className="btn btn-primary mt-3"
+                        >
+                            Create Possible Answer
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            {optionCreated && (
+                <div className="alert alert-success p-2 col-6 mx-auto">
+                    <p className="h3 text-center">
+                        Answer Successfully Created!
+                    </p>
+                    <Link to="/dashboard" className="m-3 larger-text">
+                        {"<< Go to Dashboard"}
+                    </Link>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default AddOption;
